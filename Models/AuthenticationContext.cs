9c07f7a8a78180d5784cd08f20be7bbee1aabeb2
MadihaBenAlaya <benalayamadiha@gmail.com>
2022-06-06 17:28:42 +0100

    commit1

---
+++ Models/AuthenticationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppRecrutement.Models
+{
+    public class AuthenticationContext : IdentityDbContext
+    {
+        public AuthenticationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
